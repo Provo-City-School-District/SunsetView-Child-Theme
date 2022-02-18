8617dd84ad02b553fca3d133f5a4abb33be6baeb
Espi <joshespi@gmail.com>
2022-02-18 13:25:24 -0700

    Updated Title I documents

--- sunset-child/template-parts/mega-menu-dropdowns-titlei.php
+++ sunset-child/template-parts/mega-menu-dropdowns-titlei.php
@@ -14,11 +14,9 @@
 						<ul>
 							<li class="int"><a href="https://sunset.provo.edu/title-i/what-is-title-i/">What is Title I?</a></li>
 							<li class="int"><a href="https://sunset.provo.edu/title-i/patrons-of-title-1-schools-have-the-right-to-know/">Patrons Right to Know</a></li>
-
-							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2019/09/sunset-view-school-improvement-plan-title-i-fy20.pdf">2019/2020 School Improvement Plan</a></li>
-							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2019/09/sunset-view-title-i-annual-meeting-fy20.pdf">2019/2020 Annual Title I Meeting</a></li>
-							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2019/09/sunset-view-needs-assessment-title-i-fy20.pdf">2019/2020 School Needs Assessment</a></li>
-							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2020/03/SV_Goals_1Page_fy20.pdf"><span class="hidden">Sunset View Elementary School</span> Goals At A Glance FY20</a></li>
+							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2022/01/2022-SSV-School-Improvement-Plan.pdf">2021-2022 School Improvement Plan</a></li>
+							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2022/01/2021-2022-Annual-Title-I-Meeting.pdf">2021-2022 Annual Title I Meeting</a></li>
+							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2022/01/2022-SSV-School-Needs-Assessment.pdf">2021-2022 School Needs Assessment</a></li>
 						</ul>
 						<p>Title I Coordinator: Lance Vandermark (LanceV@provo.edu)</p>
 				</div>
